cfe452e94f135d13aed064c5e34e5afd6955bed6
nikolaypleshkov <icelol@abv.bg>
2021-06-02 14:17:19 +0300

    Update app.module.ts

--- frontend/employeemanager/src/app/app.module.ts
+++ frontend/employeemanager/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {HttpClientModule} from '@angular/common/http';
+import { EmployeeService } from './employee.service';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
