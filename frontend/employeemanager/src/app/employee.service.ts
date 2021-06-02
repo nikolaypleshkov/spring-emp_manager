7223faa550f41173fc94b9aad6fd1cbbf6a5dbf0
nikolaypleshkov <icelol@abv.bg>
2021-06-02 14:17:21 +0300

    Update employee.service.ts

--- frontend/employeemanager/src/app/employee.service.ts
+++ frontend/employeemanager/src/app/employee.service.ts
@@ -1,11 +1,32 @@
+import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { Employee } from "./employee";
+import { environment } from "src/environments/environment";
 
+@Injectable({
+    providedIn: 'root'
+})
 export class EmployeeService{
-    private apiServerURL = '';
+    private apiServerURL = environment.apiBaseUrl;
 
     constructor(private http: HttpClient){}
 
-    public getEmployees(): Observable<any>{
-        return this.http.get<any>(`${this.apiServerURL}/employee/all`);
+    public getEmployees(): Observable<Employee[]>{
+        return this.http.get<Employee[]>(`${this.apiServerURL}/employee/all`);
     }
+
+    public addEmployees(employee: Employee): Observable<Employee>{
+        return this.http.post<Employee>(`${this.apiServerURL}/employee/add`, employee);
+    }
+
+    public updateEmployees(employee: Employee): Observable<Employee>{
+        return this.http.put<Employee>(`${this.apiServerURL}/employee/update`, employee);
+    }
+
+    
+    public deleteEmployees(employeeId: number): Observable<void>{
+        return this.http.delete<void>(`${this.apiServerURL}/employee/delete/${employeeId}`);
+    }
+    
 }
