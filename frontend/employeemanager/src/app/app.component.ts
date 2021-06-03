2729806cf968141f869a3d4c723ccc79740e038f
nikolaypleshkov <icelol@abv.bg>
2021-06-03 16:22:30 +0300

    Update app.component.ts

--- frontend/employeemanager/src/app/app.component.ts
+++ frontend/employeemanager/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 
@@ -9,9 +10,13 @@ import { EmployeeService } from './employee.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
- public employees!: Employee[];
-
- constructor(private employeeService: EmployeeService){}
+  employees: Employee[];
+  editEmployee!: Employee;
+  removeEmployee!: Employee;
+  
+ constructor(private employeeService: EmployeeService){
+   this.employees = [];
+ }
 
  ngOnInit(){
    this.getEmployees();
@@ -28,4 +33,71 @@ export class AppComponent implements OnInit {
      }
    );
  }
+
+ public onOpenModal(employee: Employee, mode: string): void{
+   const btn = document.createElement('button');
+   btn.type = 'button';
+   btn.style.display = 'none';
+
+   btn.setAttribute('data-toggle', 'modal');
+
+   if(mode === 'add'){
+     btn.setAttribute('data-target', '#addEmployeeModal');
+   }
+   if(mode === 'edit'){
+     this.editEmployee = employee;
+    btn.setAttribute('data-target', '#updateEmployeeModal');
+  }
+  if(mode === 'delete'){
+    this.removeEmployee = employee;
+    btn.setAttribute('data-target', '#deleteEmployeeModal');
+  }
+
+  const container = document.getElementById('container-wrapp');
+  container?.appendChild(btn);
+
+  btn.click();
+ }
+
+ public addEmployee(addForm: NgForm){
+   document.getElementById('add-employee-form')?.click();
+   this.employeeService.addEmployees(addForm.value).subscribe(
+     (response: Employee) => {
+       console.log(response);
+       this.getEmployees();
+
+       addForm.reset();
+     },
+     (error: HttpErrorResponse) => {
+       alert(error.message);
+       addForm.reset();
+    }
+   );
+ }
+
+ public updateEmployee(employee: Employee): void{
+  this.employeeService.updateEmployees(employee).subscribe(
+    (response: Employee) => {
+      console.log(response);
+      this.getEmployees();
+    },
+    (error: HttpErrorResponse) => {
+      alert(error.message)
+   }
+  );
+}
+
+public deleteEmployee(id: number): void{
+  this.employeeService.deleteEmployees(id).subscribe(
+    (response: void) => {
+      console.log(response);
+      this.getEmployees();
+    },
+    (error: HttpErrorResponse) => {
+      alert(error.message)
+   }
+  );
+}
+
+
 }
